5d467ab4fdf239c75d5ad10e368421fa72f605b2
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-05-05 11:16:53 +0200

    initaition

---
+++ src/main/java/de/neuefische/order_spring/OrderSpringApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.order_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/order_spring/OrderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.order_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
