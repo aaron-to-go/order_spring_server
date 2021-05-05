521dc1f16e573df491b2098569b6448a3324b5ff
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-05-05 16:56:40 +0200

    Add even more tests

--- src/test/java/de/neuefische/order_spring/repository/OrderRepoTest.java
+++ src/test/java/de/neuefische/order_spring/repository/OrderRepoTest.java
@@ -1,7 +1,35 @@
 package de.neuefische.order_spring.repository;
 
 
+import de.neuefische.order_spring.model.Order;
+import de.neuefische.order_spring.model.Product;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class OrderRepoTest {
 
+    @Test
+    public void addOrderAddsAnotherOrderToTheOrderMap(){
+        // GIVEN
+        OrderRepo orderRepo= new OrderRepo();
+        List<Product> productList= List.of(
+                new Product("1", "Banana"),
+                new Product("2", "Apple")
+        );
+        List<Order> expected = List.of(new Order(
+                "1", List.of(
+                        new Product("1", "Banana"),
+                        new Product("2", "Apple"))
+        ));
+
+        // WHEN
+        orderRepo.addOrder(productList);
+        List<Order> actual = orderRepo.list();
+
+        // THEN
+        assertThat(actual, is(expected));
+    }
 
 }
